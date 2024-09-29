c432ce4313c19257792ead145b648b1deeee7dd3
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-29 18:46:53 +0300

    add cdk stepper

--- src/app/checkout/checkout-review/checkout-review.component.ts
+++ src/app/checkout/checkout-review/checkout-review.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { BasketService } from '../../basket/basket.service';
 import { ToastrService } from 'ngx-toastr';
 import { CdkStepper } from '@angular/cdk/stepper';
-
+//CdkStepper is a part of Angular CDK (Component Dev Kit) that provides a low-level API for creating step-based workflows. It's used for building customizable stepper components, like multi-step forms or wizards, without depending on Material Design (as opposed to Angular Material's MatStepper).
 @Component({
   selector: 'app-checkout-review',
   templateUrl: './checkout-review.component.html',
