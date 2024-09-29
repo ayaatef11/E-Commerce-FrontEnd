826264ac31780e666bd44953abf3bb8245730014
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-29 18:38:12 +0300

    add checkout success component

---
+++ src/app/checkout/checkout-success/checkout-success.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Order } from '../../shared/interfaces/order';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-checkout-success',
+  templateUrl: './checkout-success.component.html',
+  styleUrl: './checkout-success.component.scss'
+})
+export class CheckoutSuccessComponent {
+  order?: Order;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.order = navigation?.extras?.state as Order
+  }
+}
