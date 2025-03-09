addd17e720091749e075c72f1b2306a10847b38c
ayaatef11 <ayabadrin667@gmail.com>
2025-03-10 00:38:42 +0200

    add bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 export const routes: Routes = [
    {path:'', component:HomeComponent},
   {path:'test-error', component:TestErrorComponent},
   {path:'not-found', component:NotFoundComponent},
   {path:'server-error', component:ServerErrorComponent},
+  {path:'shop', component:ShopComponent},
+
   {path:'**', redirectTo:'', pathMatch:'full'}
 ];
